1c2ff1bb691bf1f73c9d5add01a863e0ed6a01d0
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-20 17:04:11 +0100

    added CardGame class and now have a deck of cards

--- src/Card.java
+++ src/Card.java
@@ -1,7 +1,11 @@
 public class Card {
-        private String suit;
-        private String symbol;
-        private int value;
+    private final String suit;
+    private final String symbol;
+    private final int value;
+
+    private static final String[] SUITS = {"\u2665", "\u2666", "\u2663", "\u2660"};
+    private static final String[] SYMBOLS = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+    private static final int[] VALUES = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
 
         public Card(String suit, String symbol, int value) {
             this.suit = suit;
@@ -11,5 +15,29 @@ public class Card {
 
     public String toString() {
         return symbol + " of " + suit;
+    }
+
+//    public String getSuit() {
+//        return suit;
+//    }
+//
+//    public String getSymbol() {
+//        return symbol;
+//    }
+//
+//    public int getValue() {
+//        return value;
+//    }
+
+    public static String[] getSuits() {
+        return SUITS;
+    }
+
+    public static String[] getSymbols() {
+        return SYMBOLS;
+    }
+
+    public static int[] getValues() {
+        return VALUES;
     }
 }
