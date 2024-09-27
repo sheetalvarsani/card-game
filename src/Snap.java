33c7293b426ab0ca8c2326bd02bbb95149cd337a
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-27 20:23:34 +0100

    Added player class and two players to snap game

--- src/Snap.java
+++ src/Snap.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 
 public class Snap extends CardGame{
-
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
     private Card previousCard;
 
-    public Snap(String name) {
+    public Snap(String name, String player1Name, String player2Name) {
         super(name);
         this.previousCard = null;
+        this.player1 = new Player(player1Name);
+        this.player2 = new Player(player2Name);
+        this.currentPlayer = player1;
     }
-
-
 
     public void playSnap() {
         System.out.println("Let's play Snap! \nPress Enter to deal a card.");
@@ -17,7 +20,6 @@ public class Snap extends CardGame{
         Scanner scanner = new Scanner(System.in);
 
         while (true) {
-
             scanner.nextLine();
 
             Card currentCard = dealCard();
@@ -27,13 +29,17 @@ public class Snap extends CardGame{
                 break;
             }
 
-            System.out.println(currentCard);
+            System.out.println(currentPlayer.getName() + ": " + currentCard);
 
             if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
-                System.out.println("\nSNAP!\n\nGAME OVER: PLAYER WINS!");
+                System.out.println("\nSNAP!\n\nGAME OVER: " + currentPlayer.getName() + " WINS!");
+                currentPlayer.setIsWinner(true);
                 break;
             }
+
             previousCard = currentCard;
+
+            currentPlayer = (currentPlayer == player1) ? player2 : player1;
         }
         scanner.close();
     }
