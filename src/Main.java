1cad6dd4ece2eee698d21a824aa6dfe028e2d675
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-27 21:19:20 +0100

    added exit menu to play agin/exit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        Snap game = new Snap("Snap", "Player 1", "Player 2");
+        Snap game = new Snap("SNAP", "Player 1", "Player 2");
 
         game.shuffleDeck();
-
         game.playSnap();
     }
 }
--- src/Snap.java
+++ src/Snap.java
@@ -1,45 +1,67 @@
 import java.util.Scanner;
 
-public class Snap extends CardGame{
+public class Snap extends CardGame {
     private Player player1;
     private Player player2;
     private Player currentPlayer;
     private Card previousCard;
 
-    public Snap(String name, String player1Name, String player2Name) {
+    public Snap(String name, String p1Name, String p2Name) {
         super(name);
         this.previousCard = null;
-        this.player1 = new Player(player1Name);
-        this.player2 = new Player(player2Name);
+        this.player1 = new Player(p1Name);
+        this.player2 = new Player(p2Name);
         this.currentPlayer = player1;
     }
 
     public void playSnap() {
-        System.out.println("Let's play Snap! \nPress Enter to deal a card.");
-
         Scanner scanner = new Scanner(System.in);
+        boolean playAgain = true;
 
-        while (true) {
-            scanner.nextLine();
+        while (playAgain) {
+            shuffleDeck();
+            previousCard = null;
+            currentPlayer = player1;
 
-            Card currentCard = dealCard();
+            System.out.println("Let's play SNAP! \nPress Enter to deal a card.");
 
-            if(currentCard == null) {
-                System.out.println("\nGAME OVER: Nobody wins!");
-                break;
+            while (true) {
+                scanner.nextLine();
+
+                Card currentCard = dealCard();
+
+                if (currentCard == null) {
+                    System.out.println("\nGAME OVER: Nobody wins!");
+                    break;
+                }
+
+                System.out.println(currentPlayer.getName() + ": " + currentCard);
+
+                if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
+                    System.out.println("\nSNAP!\n\nGAME OVER: " + currentPlayer.getName() + " WINS!");
+                    currentPlayer.setIsWinner(true);
+                    break;
+                }
+
+                previousCard = currentCard;
+                currentPlayer = (currentPlayer == player1) ? player2 : player1;
             }
 
-            System.out.println(currentPlayer.getName() + ": " + currentCard);
+            System.out.println("\nEnter '1' to PLAY AGAIN or '2' to EXIT:");
 
-            if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
-                System.out.println("\nSNAP!\n\nGAME OVER: " + currentPlayer.getName() + " WINS!");
-                currentPlayer.setIsWinner(true);
-                break;
+            while (true) {
+                String choice = scanner.nextLine();
+                if (choice.equals("1")) {
+                    playAgain = true;
+                    break;
+                } else if (choice.equals("2")) {
+                    playAgain = false;
+                    System.out.println("Thanks for playing! BYE!");
+                    break;
+                } else {
+                    System.out.println("Please enter '1' to PLAY AGAIN or '2' to EXIT:");
+                }
             }
-
-            previousCard = currentCard;
-
-            currentPlayer = (currentPlayer == player1) ? player2 : player1;
         }
         scanner.close();
     }
