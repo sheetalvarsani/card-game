8ba98aea4e409ab185528b28e9a8cdc1ff92f2b7
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-27 18:22:28 +0100

    snap declared and game over declared when 2 cards in a row have same symbol

--- src/Snap.java
+++ src/Snap.java
@@ -1,11 +1,15 @@
 import java.util.Scanner;
 
 public class Snap extends CardGame{
+
+    private Card previousCard;
+
     public Snap(String name) {
         super(name);
+        this.previousCard = null;
     }
 
-    private final Card previousCard = null;
+
 
     public void playSnap() {
         System.out.println("Let's play Snap! \nPress Enter to deal a card.");
@@ -19,16 +23,17 @@ public class Snap extends CardGame{
             Card currentCard = dealCard();
 
             if(currentCard == null) {
-                System.out.println("Game Over!");
+                System.out.println("\nGAME OVER: Nobody wins!");
                 break;
             }
 
             System.out.println(currentCard);
 
             if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
-                System.out.println("SNAP!");
+                System.out.println("\nSNAP!\n\nGAME OVER: PLAYER WINS!");
                 break;
             }
+            previousCard = currentCard;
         }
         scanner.close();
     }
