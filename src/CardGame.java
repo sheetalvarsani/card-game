48ba31624e026760122cb6b9ac3b1bd25744a2a5
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-27 17:01:07 +0100

    a new card is dealt from user input of enter

--- src/CardGame.java
+++ src/CardGame.java
@@ -13,7 +13,7 @@ public class CardGame {
         getDeck(); // lists cards in deck
     }
 
-    private void getDeck() {
+    void getDeck() {
         for (String suit : Card.getSuits()) { // get suits through the getter
             for (int i = 0; i < Card.getSymbols().length; i++) { // get symbols through the getter
                 deckOfCards.add(new Card(suit, Card.getSymbols()[i], Card.getValues()[i])); // Add card to deck
--- src/Snap.java
+++ src/Snap.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class Snap extends CardGame{
     public Snap(String name) {
         super(name);
+    }
+
+    public void playSnap() {
+        System.out.println("Let's play Snap! Press Enter to deal a card.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            // Wait for user to press enter to deal the next card
+            scanner.nextLine();
+
+            Card currentCard = dealCard();
+
+            System.out.println("Dealt card: " + currentCard);
+        }
     }
 }
