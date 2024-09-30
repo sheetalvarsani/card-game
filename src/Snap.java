859173332d4b82c2dfd6f6fd7c82b24a33727a66
Sheetal Varsani <sheetalvarsani@hotmail.co.uk>
2024-09-30 21:14:07 +0100

    added 2 sec timer to snap game for snap call

--- src/Snap.java
+++ src/Snap.java
@@ -42,9 +42,34 @@ public class Snap extends CardGame {
                 System.out.println(currentPlayer.getName() + ": " + currentCard);
 
                 if (previousCard != null && currentCard.getSymbol().equals(previousCard.getSymbol())) {
-                    System.out.println("\nSNAP!\n\nGAME OVER: " + currentPlayer.getName() + " WINS!");
-                    currentPlayer.setIsWinner(true);
-                    break;
+                    System.out.println("\n" + currentPlayer.getName() + " Type 'SNAP' within 2 seconds to WIN!");
+
+                    Thread timerThread = new Thread (() -> {
+                        try {
+                            Thread.sleep(2000);
+                            System.out.println("\nTIME'S UP!");
+                        } catch (InterruptedException e){
+                            System.out.println("\nSNAP DECLARED!");
+                        }
+                    });
+
+                    timerThread.start();
+
+                    String snapInput = scanner.nextLine().toUpperCase();
+
+                    if (snapInput.equals("SNAP")) {
+                        timerThread.interrupt();
+                        System.out.println("\nGAME OVER: " + currentPlayer.getName() + " WINS!");
+                        currentPlayer.setIsWinner(true);
+                        break;
+                    } else {
+                        try {
+                            timerThread.join();
+                        } catch (InterruptedException _) {
+                        }
+                        System.out.println("\nGame continues.");
+                        System.out.println("It is " + currentPlayer.getName() + "'s turn.");
+                    }
                 }
 
                 previousCard = currentCard;
